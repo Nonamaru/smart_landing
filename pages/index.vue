dff729f2820ecc7f4c47808008b6c7dc9ec3c886
Nonamaru <zykinf@gmail.com>
2023-08-02 17:00:37 +0500

    metrika is worked

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-<noscript><div><img src="https://mc.yandex.ru/watch/94075822" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- <noscript><div><img src="https://mc.yandex.ru/watch/94075822" style="position:absolute; left:-9999px;" alt="" /></div></noscript> -->
 <div>
     <Head>
         <Title>Smart K Telecom</Title>
@@ -7,7 +7,7 @@
     </Head>
 </div>
 <div id="desktop">
-    <!-- <YandexMetrika /> -->
+    <YandexMetrika />
     <StartView />
     <OpportunitiesView />
     <DevicesView />
@@ -23,20 +23,20 @@ export default{
     components:{
         Icon,
     },
-    mounted() {
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-        ym(94075822, "init", {
-             clickmap:true,
-             trackLinks:true,
-             accurateTrackBounce:true,
-             webvisor:true,
-             defer: true,
-        });
-    }
+    // mounted() {
+    //     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    //     m[i].l=1*new Date();
+    //     for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    //     k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    //     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    //     ym(94075822, "init", {
+    //          clickmap:true,
+    //          trackLinks:true,
+    //          accurateTrackBounce:true,
+    //          webvisor:true,
+    //          defer: true,
+    //     });
+    // }
 }
 </script>
 <style>
